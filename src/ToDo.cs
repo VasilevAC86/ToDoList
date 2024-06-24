a0a07986fc9d2181b2df92cd4928b1c0d6af136e
Study <paulnovasharcat@gmail.com>
2024-06-24 21:17:11 +0300

    Добавьте файлы проекта.

---
+++ src/ToDo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoList.src
+{
+    public class ToDo
+    {
+        public string Title {  get; set; }
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+        public ToDo() { }
+        public ToDo(string title, DateTime date, string description) 
+        {
+            this.Date = date;
+            this.Title = title;
+            this.Description = description;
+        }
+    }
+}
